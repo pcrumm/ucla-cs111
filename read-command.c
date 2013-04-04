307014e25940c6dba74c54e10dd4a151d3a90bcf
Phillip Crumm <pcrumm@p3net.net>
2013-04-03 19:07:28 -0700

    Some annotations on what, exactly, these functions are doing. Implementing these two functions should satisfy part A of the assignment.
    @ipetkov please look over these and see if you disagree.

--- read-command.c
+++ read-command.c
@@ -11,9 +11,16 @@
 /* FIXME: Define the type 'struct command_stream' here.  This should
    complete the incomplete type declaration in command.h.  */
 
+/**
+ * Reads through a stream and assembles a command_stream by decomposing
+ * the stream into individual commands.
+ *
+ * int (*get_next_byte) (void *) - Function pointer to get_next_byte
+ * void *get_next_byte_argument - The stream to iterate through.
+ */
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
-		     void *get_next_byte_argument)
+         void *get_next_byte_argument)
 {
   /* FIXME: Replace this with your implementation.  You may need to
      add auxiliary functions and otherwise modify the source code.
@@ -22,6 +29,10 @@ make_command_stream (int (*get_next_byte) (void *),
   return 0;
 }
 
+/**
+ * Iterates over a command_stream struct and returns the next command.
+ * Sample usage: main.c:56
+ */
 command_t
 read_command_stream (command_stream_t s)
 {
