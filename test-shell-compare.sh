7db797f3d2d0c111c0f9a29bdb490db0e18f8978
Phillip Crumm <pcrumm@p3net.net>
2013-04-21 15:19:45 -0700

    Some preliminary testing for our execution functionality.
    @ipetkov two issues that need addressing:
    shell-compare/pipe.sh appears to hang in timetrash--is a pipe not being closed?
    output_redirect.sh outputs to stdout

---
+++ test-shell-compare.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+tmp=$0-$$.tmp
+mkdir "$tmp" || exit
+
+(
+cd "$tmp" || exit
+
+for file in ../shell-compare/* ; do
+  if [ ! -d "$file" ]; then
+    bash $file >bash.out 2>bash.err || exit
+    ../timetrash $file >tt.out 2>tt.err || exit
+
+    diff -u bash.out tt.out || exit
+    diff -u bash.err tt.err || exit
+  fi
+done
+) || exit
+
+cd .. || exit
+rm $tmp
